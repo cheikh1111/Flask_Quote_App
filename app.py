b2a446071bc48f1eec0eab204d7817310234f845
cheikh <cheikhsidiya55@gmail.com>
2023-06-14 22:25:16 +0000

    few updates ..

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ from random import choice
 
 app = Flask(__name__)
 def load_quote():
-    category = choice(["life", "success","amazing","cool","attitude",
-                       "computers","courage","car","change","business",
-                         "morning" , "dreams", "education","failure","family",
-                         "funny","future","graduation","food","forgiveness","great",
-                         "health","intelligence","men","government","experience",
-                         "learning" , "leadership" , "knowledge"])
+    category = choice([ "life"       ,"success"       ,"amazing"     ,
+                        "cool"       ,"attitude"      ,"computers"   ,
+                        "courage"    ,"car"           ,"change"      ,
+                        "business"   ,"morning"       , "dreams"     ,
+                        "education"  ,"failure"       ,"family"      ,
+                        "funny"      ,"future"        ,"graduation"  ,
+                        "food"       ,"forgiveness"   ,"great"       ,
+                        "health"     ,"intelligence"  ,"men"         ,
+                        "government" ,"experience"    ,"learning"    ,
+                        "leadership" ,"knowledge"     ,"learning"
+                        
+                        ])
     
     quotes_api = f'https://api.api-ninjas.com/v1/quotes?category={category}'
     response=get(quotes_api,headers={'X-Api-Key': 'q79+Z1ci+UI+QpBOFvQ8ow==yW7ecdmkRKeR5CBq'})
@@ -21,21 +27,29 @@ def load_quote():
     return quote, author,category
 
 
-
+skills=[('python',85),
+        ('Html',95),
+        ('Css',89),
+        ('Git',75)
+        ]
 
 @app.route("/")
 @app.route("/home")
 
 def home():
     quote,author,category= load_quote()
-    return render_template("home.html",title="Minute Quote",
-                           quote=quote,author=author,
-                           category=category)
+    return render_template("home.html",
+                           title="Minute Quote",
+                           quote=quote,
+                           author=author,
+                           category=category,
+                           skills=skills)
 
 @app.route("/Contact")
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", title="Contact")
+    return render_template("contact.html",
+                            title="Contact")
 
 @app.route("/source")
 @app.route("/source_code")
@@ -45,7 +59,8 @@ def source():
 
 @app.errorhandler(404)
 def e404(e):
-    return render_template('404.html', title="404 Page Not Found")
+    return render_template('404.html',
+                            title="404 Page Not Found")
     
 
 if __name__ == "__main__":
